852f745b7c1261314f2d1a73d273d998612f4668
Gustavo Pinto <ggustavohenriquepinto45@gmail.com>
2024-11-02 15:17:14 -0300

    Setups
    Para rodar streamlit e pandas

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pandas
+import streamlit
 
-def print_head(data_frame : pandas.DataFrame, lines : int = 5) -> None:
-  print(data_frame.head(lines), '\n')
+def data_frame_info(name : str, data_frame : pandas.DataFrame, lines : int = 5) -> None:
+  print(name)
+  data_frame.info()
+  print(data_frame.head(lines))
+  print()
+
+
 
 results_data_frame : pandas.DataFrame = pandas.read_csv('results.csv')
 goalscorers_data_frame : pandas.DataFrame = pandas.read_csv('goalscorers.csv')
 shootouts_data_frame : pandas.DataFrame = pandas.read_csv('shootouts.csv')
 
-print_head(results_data_frame)
-print_head(goalscorers_data_frame)
-print_head(shootouts_data_frame)
+data_frame_info("RESULTS", results_data_frame)
+data_frame_info("GOAL SCORERS", goalscorers_data_frame)
+data_frame_info("SHOOT OUTS", shootouts_data_frame)
