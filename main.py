f56c159018690bbc2a4048d0b353fc26a1a582b3
Gustavo Pinto <ggustavohenriquepinto45@gmail.com>
2024-11-02 15:35:14 -0300

    Instalação Streamlit e Pyplot

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-import pandas
-import streamlit
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
 
-def data_frame_info(name : str, data_frame : pandas.DataFrame, lines : int = 5) -> None:
+def data_frame_info(name : str, data_frame : pd.DataFrame, lines : int = 5) -> None:
   print(name)
-  data_frame.info()
+
+  # print(data_frame.isna().any())
+  # print(data_frame.shape)
+  # data_frame.info()
+  # print(data_frame.describe())
+
   print(data_frame.head(lines))
   print()
 
-
-
-results_data_frame : pandas.DataFrame = pandas.read_csv('results.csv')
-goalscorers_data_frame : pandas.DataFrame = pandas.read_csv('goalscorers.csv')
-shootouts_data_frame : pandas.DataFrame = pandas.read_csv('shootouts.csv')
+results_data_frame : pd.DataFrame = pd.read_csv('results.csv')
+goalscorers_data_frame : pd.DataFrame = pd.read_csv('goalscorers.csv')
+shootouts_data_frame : pd.DataFrame = pd.read_csv('shootouts.csv')
 
 data_frame_info("RESULTS", results_data_frame)
-data_frame_info("GOAL SCORERS", goalscorers_data_frame)
-data_frame_info("SHOOT OUTS", shootouts_data_frame)
+# data_frame_info("GOAL SCORERS", goalscorers_data_frame)
+# data_frame_info("SHOOT OUTS", shootouts_data_frame)
+
+results_grouped_by_country = results_data_frame.groupby('country').size()
+# print(results_grouped_by_country)
+
+plt.figure(figsize=(10, 6))
+results_grouped_by_country.plot(kind='barh', color='skyblue')
+plt.title('Matches amount by country')
+plt.xlabel('Matches')
+plt.ylabel('Country')
+plt.show()
