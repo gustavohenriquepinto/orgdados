30b4e7125901ffe057e8649370e6edbed2d129bf
Gustavo Pinto <ggustavohenriquepinto45@gmail.com>
2024-11-18 10:44:33 -0300

    Streamlit
    Uso da biblioteca streamlit para oferecer um dashboard interativo na visualização dos dados. Comando: streamlit run main.py

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import streamlit as st
+import datetime
 
-def data_frame_info(name : str, data_frame : pd.DataFrame, lines : int = 5) -> None:
-  print(name)
+# results_base = pd.read_csv('results.csv')
+# goalscorers_base = pd.read_csv('goalscorers.csv')
+# shootouts_base = pd.read_csv('shootouts.csv')
 
-  # print(data_frame.isna().any())
-  # print(data_frame.shape)
-  # data_frame.info()
-  # print(data_frame.describe())
+# results_conditions = None
+# goalscorers_conditions = None
+# shootouts_conditions = None
 
-  print(data_frame.head(lines))
-  print()
+results_df = pd.read_csv('results.csv')
+goalscorers_df = pd.read_csv('goalscorers.csv')
+shootouts_df = pd.read_csv('shootouts.csv')
 
-results_data_frame : pd.DataFrame = pd.read_csv('results.csv')
-goalscorers_data_frame : pd.DataFrame = pd.read_csv('goalscorers.csv')
-shootouts_data_frame : pd.DataFrame = pd.read_csv('shootouts.csv')
+initial_date = st.sidebar.date_input('Início', min_value=datetime.date(1872,1,1), max_value=datetime.date(2024,12,31))
+final_date = st.sidebar.date_input('Fim', min_value=datetime.date(1872,1,1), max_value=datetime.date(2024,12,31))
 
-data_frame_info("RESULTS", results_data_frame)
-# data_frame_info("GOAL SCORERS", goalscorers_data_frame)
-# data_frame_info("SHOOT OUTS", shootouts_data_frame)
+# best_shootout_winner = 
 
-results_grouped_by_country = results_data_frame.groupby('country').size()
-# print(results_grouped_by_country)
-
-plt.figure(figsize=(10, 6))
-results_grouped_by_country.plot(kind='barh', color='skyblue')
-plt.title('Matches amount by country')
-plt.xlabel('Matches')
-plt.ylabel('Country')
-plt.show()
